464577702c31dba11a19375950f92ff44c6fb019
elisad5791 <elisad5791@mail.ru>
2022-03-28 19:56:05 +0500

    Add webpack, bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/elisad5791/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/elisad5791/frontend-project-lvl3/actions)
-### Hello
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
