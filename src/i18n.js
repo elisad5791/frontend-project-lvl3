10c512fa877dd5952898bd71c722397345d2035c
elisad5791 <elisad5791@mail.ru>
2022-03-30 01:04:02 +0500

    Add layout for posts

--- src/i18n.js
+++ src/i18n.js
@@ -14,6 +14,8 @@ i18next
 
 document.getElementById('main-heading').innerHTML = i18next.t('main-heading');
 document.getElementById('sub-heading').innerHTML = i18next.t('sub-heading');
+document.getElementById('heading-posts').innerHTML = i18next.t('heading-posts');
+document.getElementById('heading-feeds').innerHTML = i18next.t('heading-feeds');
 document.getElementById('example').innerHTML = i18next.t('example');
 document.getElementById('button-add').innerHTML = i18next.t('button-add');
 document.getElementById('url').placeholder = i18next.t('input-placeholder');
--- src/index.js
+++ src/index.js
@@ -45,10 +45,37 @@ axios.get('https://ru.hexlet.io/lessons.rss')
   .then(function (response) {
     const parser = new DOMParser();
     const doc = parser.parseFromString(response.data, "application/xml");
-    console.log(doc);
     const result = [...doc.getElementsByTagName('channel')[0].childNodes];
-    const output = result.filter((item) => item.nodeName === 'title');
-    console.log(output[0].textContent);
+    
+    const titleNodes = result.filter((item) => item.nodeName === 'title');
+    const title = titleNodes[0].textContent;
+    const descNodes = result.filter((item) => item.nodeName === 'description');
+    const description = descNodes[0].textContent;
+
+    const titleElem = document.createElement('p');
+    titleElem.classList.add('fw-bold', 'mb-0');
+    titleElem.textContent = title;
+    const descElem = document.createElement('p');
+    descElem.textContent = description;
+    const feeds = document.getElementById('feeds');
+    feeds.append(titleElem, descElem);
+
+    const posts = document.getElementById('posts');
+    const itemNodes = result.filter((item) => item.nodeName === 'item');
+    itemNodes.forEach((itemNode) => {
+      const link = document.createElement('a');
+      link.textContent = itemNode.getElementsByTagName('title')[0].textContent;
+      link.href = itemNode.getElementsByTagName('link')[0].textContent;
+
+      const button = document.createElement('button');
+      button.classList.add('btn', 'btn-outline-primary');
+      button.textContent = 'Просмотр';
+
+      const div = document.createElement('div');
+      div.classList.add('d-flex', 'justify-content-between', 'mb-3');
+      div.append(link, button);
+      posts.append(div);
+    });
   })
   .catch(function (error) {
     console.log(error);
